f106e1d149d45c3391dca33ae309ef287b19e535
Mahsa Farahani <38652406+mahsafrhni@users.noreply.github.com>
2021-01-27 21:10:05 +0330

    Update Notifications.js

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,27 +1,58 @@
 import React from 'react';
 import '../css/notifications.css';
 import {Link} from "react-router-dom";
+import logo from "../src/logo.png";
 
 class Notifications extends React.Component {
 
     render() {
         return (
-            <div className="App">
-                <ul>
-                    <Link to="/home"><li className="sidebar">Home</li></Link>
-                    <Link to="/notifications"><li className="sidebar">Notifications</li></Link>
-                    <Link to="/notifications"><li className="sidebar">Profile</li></Link>
-                </ul>
-                <button id="tweet_button">Tweet</button>
-                <h2 id="notifications">Notifications</h2>
-                <ul className="pagination">
-                    <a href="#">All</a>
-                  <a className="active" href="#">mentions</a>
-                </ul>
-
-                <div className="search">
-                    <input type="text" size="10" placeholder="Search Twitter" name="search"></input>
-                </div>
+            <div>
+                <table id="main_table">
+                    <tr class="row">
+                        <td class="col" colSpan="4">
+                            <h2 id="home">Twitter</h2>
+                        </td>
+                    </tr>
+                    <tr class="row">
+                        <td class="col-4">
+                            <div id="sidebar_left">
+                                <img id="logo-home" src={logo} alt="logo"></img>
+                                <Link to="/home">
+                                    <button class="btn btn-outline-primary menu-item">Home</button>
+                                </Link>
+                                <br/>
+                                <Link to="/notifications">
+                                    <button class="btn btn-outline-primary menu-item">Notifications</button>
+                                </Link>
+                                <br/>
+                                <Link to="/profile">
+                                    <button class="btn btn-outline-primary menu-item">Profile</button>
+                                </Link>
+                                <Link to="/profile">
+                                    <button class="btn btn-outline-primary menu-item">Log out</button>
+                                </Link>
+                                <br/>
+                                {/*<button id="tweet_button">Tweet</button>*/}
+                            </div>
+                        </td>
+                        <td class="col-5" colSpan="2">
+                            <div id="page_center">
+                                <ul className="pagination">
+                                    <a href="#">All</a>
+                                    <a className="active" href="#">mentions</a>
+                                </ul>
+                            </div>
+                        </td>
+                        <td class="col-3">
+                            <div id="sidebar_right">
+                                <input id="search_txt" type="text" size="10" class="form-control"
+                                       placeholder="Search Twitter"
+                                       name="search"></input>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
             </div>
         );
     }
