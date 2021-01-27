b8f11ac420ea0bc48c8ccdcf221c0e6c78b6d78c
Mahsa Farahani <38652406+mahsafrhni@users.noreply.github.com>
2021-01-27 16:04:54 +0330

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,21 +2,39 @@ import React from 'react';
 import '../css/profile.css';
 import {Link} from "react-router-dom";
 import avatar from '../src/avatar.png';
-class Profile extends React.Component {
 
+class Profile extends React.Component {
+    handleFileUpload = event => {
+        console.log(event.target.files[0].name);
+    };
     render() {
         return (
             <div className="App">
                 <ul>
-                    <Link to="/home"><li className="sidebar">Home</li></Link>
-                    <Link to="/notifications"><li className="sidebar">Notifications</li></Link>
-                    <Link to="/notifications"><li className="sidebar">Profile</li></Link>
+                    <Link to="/home">
+                        <li className="sidebar">Home</li>
+                    </Link>
+                    <Link to="/notifications">
+                        <li className="sidebar">Notifications</li>
+                    </Link>
+                    <Link to="/notifications">
+                        <li className="sidebar">Profile</li>
+                    </Link>
                 </ul>
                 <button id="tweet_button">Tweet</button>
                 <h2 id="profile">Profile</h2>
                 <button id="edit_photo">Edit profile photo</button>
                 <button id="edit_id">Edit username</button>
-                <img src={avatar} alt="Avatar" className="avatar"></img>
+                <React.Fragment>
+                    <input
+                        ref="fileInput"
+                        onChange={this.handleFileUpload}
+                        type="file"
+                        style={{display: "none"}}
+                        // multiple={false}
+                    />
+                    <img onClick={() => this.refs.fileInput.click()}
+                         src={avatar} alt="Avatar" className="avatar"></img> </React.Fragment>
                 <h1 id="followers">0 followers</h1>
                 <h1 id="followings">0 followings</h1>
                 <div className="search">
